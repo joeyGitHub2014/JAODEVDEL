1bd1ea46bb6fd3585f2971f4f8f72d2dcfa85afd
joeyGitHub2014 <jorlando2012@gmail.com>
2020-11-02 12:10:56 -0800

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #ff6e17;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
